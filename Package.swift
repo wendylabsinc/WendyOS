af3c15b3e78a81f9a04657aefbb8571a586971ce
Joannis Orlandos <joannis@orlandos.nl>
2026-01-13 20:01:15 +0100

    Re-enable macOS 15 builds

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "wendy-agent",
     platforms: [
-        .macOS(.v26)
+        .macOS(.v15)
     ],
     products: [
         .executable(name: "wendy-agent", targets: ["wendy-agent"]),
