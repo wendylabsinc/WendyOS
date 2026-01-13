75fe6b26298b88b2c6325d327d96fd1bb03359ad
Joannis Orlandos <joannis@orlandos.nl>
2026-01-13 10:24:44 +0100

    Update wendy device update path

--- Scripts/dev-update-agent.sh
+++ Scripts/dev-update-agent.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-swiftly run swift build --scratch-path .agent-build --product wendy-agent --swift-sdk aarch64-swift-linux-musl && wendy device update --binary .build/aarch64-swift-linux-musl/debug/wendy-agent
+swiftly run swift build --scratch-path .agent-build --product wendy-agent --swift-sdk aarch64-swift-linux-musl && wendy device update --binary .agent-build/aarch64-swift-linux-musl/debug/wendy-agent
