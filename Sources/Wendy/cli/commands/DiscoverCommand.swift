c290d71c081f4996e358cd51cdaf92307d12c0dd
Joannis Orlandos <joannis@orlandos.nl>
2026-01-16 12:34:01 +0100

    Show BLE LocalName when discovering

--- Sources/Wendy/cli/commands/DiscoverCommand.swift
+++ Sources/Wendy/cli/commands/DiscoverCommand.swift
@@ -260,7 +260,7 @@ struct DiscoverCommand: AsyncParsableCommand {
 
                         let device = BluetoothDevice(
                             id: result.peripheral.id.rawValue,
-                            displayName: result.advertisementData.localName ?? "WendyOS Device",
+                            displayName: result.advertisementData.localName ?? result.peripheral.name ?? "WendyOS Device",
                             address: result.peripheral.id.rawValue,
                             rssi: result.rssi,
                             l2capPSM: l2capPSM
