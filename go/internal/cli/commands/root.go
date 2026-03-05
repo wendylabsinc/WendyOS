5bab8512af38b7c7627eb95809f4a3cea65b5ed7
Joannis Orlandos <joannis@orlandos.nl>
2026-03-05 23:27:43 +0100

    Clean up duplicate command

--- go/internal/cli/commands/root.go
+++ go/internal/cli/commands/root.go
@@ -88,9 +88,6 @@ func NewRootCmd() *cobra.Command {
 	bluetoothCmd.GroupID = "devices"
 	hardwareCmd := newHardwareCmd()
 	hardwareCmd.GroupID = "devices"
-	telemetryCmd := newTelemetryCmd()
-	telemetryCmd.GroupID = "devices"
-
 	// Misc Commands
 	cacheCmd := newCacheCmd()
 	cacheCmd.GroupID = "misc"
@@ -114,7 +111,6 @@ func NewRootCmd() *cobra.Command {
 		audioCmd,
 		bluetoothCmd,
 		hardwareCmd,
-		telemetryCmd,
 		cacheCmd,
 		updateCmd,
 		infoCmd,
